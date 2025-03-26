7e1f5d96a06e7ff46083c000aac538008cf02396
pradhyum999-cyber <pradhum101@gmail.com>
2025-03-26 13:41:29 +0530

    Updates

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "build"),
     emptyOutDir: true
   }
 });
